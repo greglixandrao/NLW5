8e904023544884ad8d24e0abae5e0ec102ece197
Greg Lixandrão <contato@greglixandrao.com>
2021-04-25 20:48:56 -0300

    Final Code with bugs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { http } from "./http";
 import "./websocket/client";
+import "./websocket/admin";
 
 const PORT = 3333;
 http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
